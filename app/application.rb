07610007ee4f02cf4fb052c26960ae3cc2ad871c
Dan Foley <dan@dan-foley.com>
2020-03-29 22:48:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -22,9 +22,9 @@ class Application
       # if it's in the items list then add it to the cart
       if @@items.include?(new_item.capitalize)
         @@cart << new_item.capitalize
-        resp.write "#{new_item} was added to cart!"
+        resp.write "#{new_item.capitalize} was added to cart!"
       else
-        resp.write "#{new_item} is not included in our inventory"
+        resp.write "#{new_item.capitalize} is not included in our inventory"
       end
 
     elsif req.path.match(/cart/)
