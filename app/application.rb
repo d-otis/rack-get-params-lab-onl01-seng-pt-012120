25f3fa72396402e511fe6461d34f3540e7e400dd
Dan Foley <dan@dan-foley.com>
2020-03-29 22:42:04 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,7 +26,7 @@ class Application
         resp.write "#{item} is not included in our inventory"
 
     elsif req.path.match(/cart/)
-      @@cart.empty? ? resp.write "Your cart is empty" : @@cart.each {|item| resp.write "{item}\n"}
+      # @@cart.empty? ? resp.write "Your cart is empty" : @@cart.each {|item| resp.write "{item}\n"}
     else
       resp.write "Path Not Found"
     end
